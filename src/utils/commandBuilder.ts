2f32304732a61a853dfba8f0ce50d6aa0c8bc9b9
Breezist <wafflorus@gmail.com>
2026-08-01 16:48:56 -0400

    A

--- src/utils/commandBuilder.ts
+++ src/utils/commandBuilder.ts
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder, Client, type Interaction, Collection } from 'discord.js'
+import { SlashCommandBuilder, Client, type Interaction, Collection, ChatInputCommandInteraction } from 'discord.js'
 
 export class commandBuilder {
     static client : Client;
@@ -10,7 +10,15 @@ export class commandBuilder {
     name : string = `temp`!;
     description : string = `Temporary description goes here!`!;
     callback : (...any: any) => {};
-    category : string = "";
+    category : string = ``;
+
+    options: Array<{
+        type: `string`,
+        name: string,
+        description: string,
+        required: boolean,
+        choices?: { name: string; value: string }[] | undefined
+    }> = [];
 
     constructor(name : string, description : string, callback : (...any: any) => {}) {
         let commandName = this.name = (name ? name : this.name).toLowerCase();
@@ -18,10 +26,58 @@ export class commandBuilder {
         this.callback = callback;
     }
 
+    addStringOption(
+        name: string,
+        description: string,
+        required: boolean = false,
+        choices?: { name: string, value: string }[]
+    ) {
+        this.options.push({
+            type: `string`,
+            name,
+            description,
+            required,
+            choices
+        });
+        return this;
+    }
+
+    getInteractionOptions(interaction: ChatInputCommandInteraction) {
+        const result: Record<string, string | null> = {};
+
+        for (const opt of this.options) {
+            if (opt.type === `string`) {
+                result[opt.name] = interaction.options.getString(opt.name);
+            }
+        }
+
+        return result;
+    }
+
     build() {
-        return new SlashCommandBuilder()   
+        const builder = new SlashCommandBuilder()   
             .setName(this.name)
             .setDescription(this.description)
+        
+        for (const opt of this.options) {
+            if (opt.type === `string`) {
+                builder.addStringOption(o => {
+                    o.setName(opt.name)
+                     .setDescription(opt.description)
+                     .setRequired(opt.required);
+
+                    if (opt.choices?.length) {
+                        for (const choice of opt.choices) {
+                            o.addChoices({ name: choice.name, value: choice.value });
+                        }
+                    }
+
+                    return o;
+                });
+            }
+        }
+
+        return builder;
     }
 
     setCategory(category : string) {
