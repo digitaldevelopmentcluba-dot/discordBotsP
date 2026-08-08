1c1c82e876f1c3d38b8a5f964055df6b0ee9e895
Breezist <wafflorus@gmail.com>
2026-08-08 00:29:58 -0400

    Fixed!

---
+++ src/commands/ping.ts
@@ -0,0 +1,11 @@
+import {commandBuilder} from '#utils/commandBuilder.js';
+import { type Interaction, type ChatInputCommandInteraction, type ActionRow, MessageFlags, ActionRowBuilder } from 'discord.js';
+import { buttonBuilder } from '#utils/components.js';
+
+let command = new commandBuilder(`ping`, `Pings the bot!`, async function(interaction : ChatInputCommandInteraction) {
+    let start = (Date.now());
+    let message = await interaction.deferReply();
+    message.edit({content: `🏓 Pong! (${(Date.now() - start)}ms)`});
+});
+
+export { command };
---
+++ src/events/interactionCreate.ts
@@ -0,0 +1,40 @@
+import { Events, CommandInteraction, MessageFlags, type Interaction } from 'discord.js';
+import {commandBuilder} from '#utils/commandBuilder.js'
+import { buttonBuilder, modalBuilder } from '#utils/components.js';
+
+export default {
+    event: Events.InteractionCreate,
+    async execute(interaction : Interaction, ...args : any) {
+        if (interaction.isModalSubmit()) {
+            return modalBuilder.handle(interaction);
+        }
+        
+        if (interaction.isButton()) {
+            return buttonBuilder.handle(interaction);
+        }
+
+        if (interaction.isChatInputCommand()) {
+            const command = interaction.client.commands.get(interaction.commandName);
+            if (!command) {
+                console.error(`No command matching ${interaction.commandName} was found.`);
+                return;
+            }
+            try {
+                await command.callback(interaction);
+            } catch (error) {
+                console.error(error);
+                if (interaction.replied || interaction.deferred) {
+                    await interaction.followUp({
+                        content: 'There was an error while executing this command!',
+                        flags: MessageFlags.Ephemeral,
+                    });
+                } else {
+                    await interaction.reply({
+                        content: 'There was an error while executing this command!',
+                        flags: MessageFlags.Ephemeral,
+                    });
+                }
+            }
+        }
+    }
+}
