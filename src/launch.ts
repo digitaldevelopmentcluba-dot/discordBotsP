a76f39735430c6b307d5a4be9d56ef7a5522d223
Breezist <wafflorus@gmail.com>
2026-08-08 00:37:39 -0400

    Fixed

--- src/launch.ts
+++ src/launch.ts
@@ -22,7 +22,7 @@ app.use(expressLayouts);
 app.set(`layout`, `layouts/main`);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
-app.use(express.static(path.join(process.cwd(), `src`, `web`, `views`, 'public')));
+app.use(express.static(path.join(process.cwd(), `src`, `web`, 'public')));
 
 let manager = new shardManager(path.join(__dirname, `./bot.js`), process.env.token);
 await manager.init();
