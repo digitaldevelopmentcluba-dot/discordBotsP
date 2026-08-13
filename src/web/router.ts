3e70a81f1c7005d71f42ef0b078cf7555efb8146
Breezist <wafflorus@gmail.com>
2026-08-13 19:21:20 -0400

    A

--- src/web/router.ts
+++ src/web/router.ts
@@ -2,7 +2,6 @@ import { type Router } from "express";
 import crypto from "crypto";
 import { addNewsPost, getNewsPostById, getAllNewsPosts, validateNewsPost } from "#utils/core/news.js"
 import { isLoggedIn, getAvatarUrl } from "#utils/core/passport.js"
-import { marked } from 'marked';
 
 export function extractSummary(content : any) {
   if (!content || typeof content !== `string`) return ``;
@@ -97,14 +96,9 @@ export function setupRouter(router: Router, self?: any) {
       });
     }
 
-    const renderedContent = marked(post.content);
-
     res.render(`post`, {
       title: post.title,
-      post: {
-        ...post,
-        renderedContent
-      }
+      post
     });
   });
 
